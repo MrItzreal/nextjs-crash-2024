8121c11eeb1f177c16baf13a52fc8feb34ae1a6d
Israel Santana <is.wsie@gmail.com>
2024-08-16 16:08:08 -0400

    Learned Basic MongoDB connection:
    -To implement basic MongoDB connection logic with status tracking and error handling.

---
+++ utils/database.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let isConnected = false; //Tracks connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true); // Sets mongoose options
+
+  if (isConnected) {
+    console.log("MongoDB is already connected!");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB is connected!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Note: If you don't set "mongoose.set" you'll get warnings in the console.
