bd6ba504d75cd353314ce861902f06b51ce0c67f
Israel Santana <is.wsie@gmail.com>
2024-09-04 11:27:56 -0400

    Completed the boiler plate for the POST request that will be use to create new prompts into the application. Next step, create API endpoints.

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -11,7 +11,29 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const res = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
